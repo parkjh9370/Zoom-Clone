f0583473b06a92bd106b1bc2481299c00f8a1639
Jaehyeon <parkjh9370@naver.com>
2022-03-31 23:06:43 +0900

    connect webSocket

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,1 +1,3 @@
-alert("hi")
+// socket : 연결된 서버 메세지 수신
+// window.location.host : 현재 브라우저 주소 위치 (localhost:3000)
+const backSocket = new WebSocket(`ws://${window.location.host}`)
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import { WebSocketServer } from "ws";
 import express from "express";
 
 const app = express();
@@ -11,4 +13,20 @@ app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+
+//http server
+const server = http.createServer(app)
+
+// Websocket server + http server
+// http 서버에서 websocket 서버를 만들 수 있음
+// http://localhost:3000 + ws//localhost:3000
+// 즉 같은 포트에서 2개의 프로토콜(http, ws) 동작 
+const frontSocket = new WebSocketServer({ server });
+
+// 콜백으로 전달되는 (socket) : 연결된 브라우저 메세지 수신
+function handleConnection(socket) {
+    console.log(socket);
+}
+frontSocket.on("connection", handleConnection);
+
+server.listen(3000, handleListen);
