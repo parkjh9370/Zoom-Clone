f967f51ac4174f81575d2df3a0bba7b2683fc0ea
Jaehyeon <parkjh9370@naver.com>
2022-04-02 14:43:46 +0900

    [Websocket]complete chat

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,3 +1,63 @@
 // socket : 연결된 서버 메세지 수신
 // window.location.host : 현재 브라우저 주소 위치 (localhost:3000)
-const backSocket = new WebSocket(`ws://${window.location.host}`)
+const socket = new WebSocket(`ws://${window.location.host}`)
+ 
+// socket.addEventListener("open", () => {
+//     console.log("Connected to Server 😀 ");
+// });
+
+// socket.addEventListener("message", (message) => {
+//     console.log("New Message: ", message.data );
+// })
+
+//10 초 후 서버로 해당 메세지 전송
+// setTimeout(() => {
+//     socket.send("hello from the brwoser!");
+// }, 10000)
+
+// socket.addEventListener("close", () => {
+//     console.log("Disconnected from Server ❌ ");
+// });
+
+socket.onopen = () => {
+    console.log("Connected to the server 😀 ");
+    };
+
+socket.onmessage = (message) => {
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    messageList.append(li);
+};
+
+socket.onclose = () => {
+    console.log(`Disconnected from the server ❌ `);
+};
+
+// --------------------------------------------------
+
+const messageList = document.querySelector("ul");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
+
+function makeMessage(type, payload) {
+    const msg = { type, payload };
+    return JSON.stringify(msg);
+}
+
+function handleNickSubmit(event) {
+    event.preventDefault();
+    const input = nickForm.querySelector("input");
+    socket.send(makeMessage("nickname", input.value));
+    input.value = '';
+}
+
+function handleSubmit(event) {
+    // submit 이벤트 사용 시 새로고침 되는 현상 막아줌
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    socket.send(makeMessage("new_message", input.value))
+    input.value = '';
+}
+
+nickForm.addEventListener("submit", handleNickSubmit)
+messageForm.addEventListener("submit", handleSubmit)
--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json())
 app.set("view engine", "pug");
 app.set('views', __dirname + "/views");
 // public url 생성해 해당 파일 유저에게 공유
@@ -11,9 +12,9 @@ app.set('views', __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
-
+ 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-
+ 
 //http server
 const server = http.createServer(app)
 
@@ -21,12 +22,40 @@ const server = http.createServer(app)
 // http 서버에서 websocket 서버를 만들 수 있음
 // http://localhost:3000 + ws//localhost:3000
 // 즉 같은 포트에서 2개의 프로토콜(http, ws) 동작 
-const frontSocket = new WebSocketServer({ server });
+const socket = new WebSocketServer({ server });
+
+const sockets = [];
 
 // 콜백으로 전달되는 (socket) : 연결된 브라우저 메세지 수신
-function handleConnection(socket) {
-    console.log(socket);
-}
-frontSocket.on("connection", handleConnection);
+socket.on("connection", (socket) => {
+    // 연결된 브라우저(크롬, 파이어폭스 등) 배열안에 넣기
+    sockets.push(socket);
+    // 채팅 닉네임 디폴트 설정
+    socket["nickname"] = "Anonymous";
+    console.log("Connected to Browser 😀 ");
+    // 브라우저 창 닫혔을 때 실행
+    socket.on("close", () => {
+        console.log("Disconnected from Browser ❌ ")
+        }
+    )
+
+    socket.on("message", msg => {
+        // 연결된 부라우저 모두에게 메세지 보내주기
+        // console.log(message.toString("utf-8"));
+        const message = JSON.parse(msg)
+        // console.log(message)
+        switch (message.type) {
+            case "new_message":
+                sockets.forEach(aSocket => aSocket.send(`${socket.nickname}: ${message.payload}`));
+                break
+            case "nickname":
+                socket["nickname"] = message.payload;
+                break
+        }  
+        // 브라우저에서 받아온 메세지 돌려주기
+        // socket.send(message.toString('utf-8'));
+    })
+    
+});
 
 server.listen(3000, handleListen);
